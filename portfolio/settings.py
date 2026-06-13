3e2a2b79cc2bff1395962af4c1c7cc95d7238ca3
Egor <egorik24@gmail.com>
2026-06-13 14:19:41 +0500

    Add project screenshots

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,3 +123,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'goka4ka2008@gmail.com'
 EMAIL_HOST_PASSWORD = 'mfanknytgjmbkuhr'
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
